94da4a9f14e494b4895c1ef44fb340f93fa55c73
err-arman <err.arman@gmail.com>
2025-04-30 19:45:44 +0600

    added loading and env file

--- src/pages/watch-tv/WatchTv.tsx
+++ src/pages/watch-tv/WatchTv.tsx
@@ -4,28 +4,45 @@ import { VideoPlayer } from "@/components/VideoPlayer/VideoPlayer";
 import { parseM3U } from "@/components/Utility/ParseM3u";
 import logo from "/afrokaviar.png";
 import Footer from "@/components/Footer";
+import { Loader } from "lucide-react";
+import { useToast } from "@/components/ui/use-toast";
 
 export default function WatchTv() {
+  const { toast } = useToast();
   const [channels, setChannels] = useState<any[]>([]);
   const [displayedChannels, setDisplayedChannels] = useState<any[]>([]);
   const [visibleCount, setVisibleCount] = useState(20);
   const [selectedChannel, setSelectedChannel] = useState<any>(null);
+  const [loading, setLoading] = useState(false);
 
   const [search, setSearch] = useState("");
   const [group, setGroup] = useState("All");
 
   const fetchChannels = async () => {
-    const response = await fetch("https://iptv-org.github.io/iptv/index.m3u");
-    const text = await response.text();
-    const parsed = parseM3U(text);
-    const filteredChannels = parsed
-      .filter((channel) => channel.group !== "Undefined")
-      .map((channel) => ({
-        ...channel,
-        // Take only the first category before semicolon
-        group: channel.group.split(";")[0].trim(),
-      }));
-    setChannels(filteredChannels);
+    setLoading(true);
+    try {
+      const response = await fetch(import.meta.env.VITE_TV_CHANNEL_API);
+      const text = await response.text();
+
+      const parsed = parseM3U(text);
+      const filteredChannels = parsed
+        .filter((channel) => channel.group !== "Undefined")
+        .map((channel) => ({
+          ...channel,
+          // Take only the first category before semicolon
+          group: channel.group.split(";")[0].trim(),
+        }));
+      setChannels(filteredChannels);
+      setLoading(false);
+    } catch (error) {
+      setLoading(true);
+      toast({
+        color: "red",
+        title: "Something went wrong",
+        description: "Please try again later.",
+      });
+      console.error("Error fetching channels:", error);
+    }
   };
 
   useEffect(() => {
@@ -49,15 +66,17 @@ export default function WatchTv() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  const filtered = channels.filter((c) => {
-    return (
-      (group === "All" || c.group === group) &&
-      c.name.toLowerCase().includes(search.toLowerCase())
-    );
-  });
+  const filtered =
+    channels?.length &&
+    channels?.filter((c) => {
+      return (
+        (group === "All" || c.group === group) &&
+        c.name.toLowerCase().includes(search.toLowerCase())
+      );
+    });
 
   useEffect(() => {
-    setDisplayedChannels(filtered.slice(0, visibleCount));
+    if (filtered) setDisplayedChannels(filtered.slice(0, visibleCount));
   }, [filtered, visibleCount]);
 
   const uniqueGroups = ["All", ...new Set(channels.map((c) => c.group))];
@@ -66,12 +85,10 @@ export default function WatchTv() {
     <div className="bg-black text-white min-h-screen flex flex-col">
       <div className="px-2 pt-5">
         <div className="max-w-5xl mx-auto text-center mb-5">
-          <h1 className="text-4xl font-bold">
-            <h1 className="mb-1">
-              <img src={logo} alt={logo} className="w-80 h-14 mx-auto mb-10" />
-            </h1>
-            Live African Channels
+          <h1 className="mb-1 text-4xl font-bold">
+            <img src={logo} alt={logo} className="w-80 h-14 mx-auto mb-10" />
           </h1>
+          <h1 className="mb-1 text-4xl font-bold">Live African Channels</h1>
           <h1 className="text-gray-300 text-xl mt-4 mb-4">
             Watch real stories. Real time.
           </h1>
@@ -81,32 +98,43 @@ export default function WatchTv() {
           </p>
         </div>
 
+        {loading ? (
+          <div className="flex justify-center items-center h-40">
+            <Loader className="animate-spin h-10 w-10 text-white" />
+          </div>
+        ) : null}
+
         {/* Filters */}
-        <div className="max-w-5xl mx-auto mb-6 flex flex-col sm:flex-row items-center justify-between gap-4">
-          <input
-            type="text"
-            placeholder="Search channels..."
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            className="w-full sm:w-1/2 p-2 bg-gray-800 text-white rounded-lg border border-gray-600"
-          />
-          <select
-            value={group}
-            onChange={(e) => {
-              setGroup(e.target.value);
-              setVisibleCount(20);
-            }}
-            className="w-full sm:w-1/3 p-2 bg-gray-800 text-white rounded-lg border border-gray-600"
-          >
-            {uniqueGroups.map((g, i) => (
-              <option key={i} value={g}>
-                {g}
-              </option>
-            ))}
-          </select>
-        </div>
+        {loading ? (
+          ""
+        ) : (
+          <div className="max-w-5xl mx-auto mb-6 flex flex-col sm:flex-row items-center justify-between gap-4">
+            <input
+              type="text"
+              placeholder="Search channels..."
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              className="w-full sm:w-1/2 p-2 bg-gray-800 text-white rounded-lg border border-gray-600"
+            />
+            <select
+              value={group}
+              onChange={(e) => {
+                setGroup(e.target.value);
+                setVisibleCount(20);
+              }}
+              className="w-full sm:w-1/3 p-2 bg-gray-800 text-white rounded-lg border border-gray-600"
+            >
+              {uniqueGroups.map((g, i) => (
+                <option key={i} value={g}>
+                  {g}
+                </option>
+              ))}
+            </select>
+          </div>
+        )}
 
         {/* Channel Grid */}
+
         <div
           className="max-w-5xl mx-auto overflow-y-auto mb-8"
           style={{
@@ -122,7 +150,7 @@ export default function WatchTv() {
             }
           `}</style>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 max-w-5xl mx-auto">
-            {displayedChannels.map((channel, index) => (
+            {displayedChannels?.map((channel, index) => (
               <div
                 key={index}
                 onClick={() => setSelectedChannel(channel)}
